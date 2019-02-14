e776a46c96a2d188b6ba8a1811de9f18b9f9f311
Laurent Vaylet <laurent.vaylet@renault.com>
2019-02-14 15:16:54 +0100

    add instructions on how to access the Kubeflow pipelines UI

--- 04_start_kf_ui.sh
+++ 04_start_kf_ui.sh
@@ -4,3 +4,4 @@ export NAMESPACE=kubeflow
 
 kubectl port-forward -n ${NAMESPACE} $(kubectl get pods -n ${NAMESPACE} --selector=service=ambassador -o jsonpath='{.items[0].metadata.name}') 8085:80
 
+# Now visit http://localhost:8085 to display the Kubeflow UI, or http://localhost:8085/pipeline to display the Kubeflow pipelines
