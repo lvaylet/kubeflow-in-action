b9f5d1fc1283a42e71935330817165a47d30080e
Laurent Vaylet <laurent.vaylet@renault.com>
2019-02-15 15:34:48 +0100

    create App Engine application in europe-west3

--- containers/deployapp/deploy.sh
+++ containers/deployapp/deploy.sh
@@ -12,7 +12,8 @@ export MODEL_VERSION=$2
 cd /babyweight/src/training-data-analyst/courses/machine_learning/deepdive/06_structured/serving/application
 
 pip install -r requirements.txt -t lib
-gcloud app create --region europe-west1
+# Create App Engine application. Available regions can be listed with: gcloud app regions list
+gcloud app create --region europe-west3
 gcloud app deploy
 
 # output
